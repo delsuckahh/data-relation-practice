62f296996894e3763722effc019f47b13f86f28e
delsuckahh <zdelcoco@gmail.com>
2019-01-29 21:32:53 -0600

    init

---
+++ DataRelationPractice/Data/Migrations/20190130025302_view.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataRelationPractice.Data.Migrations
+{
+    public partial class view : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recipe_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipe_UserId",
+                table: "Recipe",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipe");
+        }
+    }
+}
---
+++ DataRelationPractice/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DataRelationPractice.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public IdentityUser User { get; set; }
+
+    }
+}
