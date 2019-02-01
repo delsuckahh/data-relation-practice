4960e297f922847a9a1f787b25570fecd387618c
delsuckahh <zdelcoco@gmail.com>
2019-01-31 21:07:35 -0600

    day 11

--- DataRelationPractice/Models/Recipe.cs
+++ DataRelationPractice/Models/Recipe.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace DataRelationPractice.Models
 {
@@ -13,5 +15,21 @@ namespace DataRelationPractice.Models
 
     public class ApplicationUser : IdentityUser
     {
+        //public int UserId { get; set; }
+
+        //public string DisplayUserName { get; set; }
+
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        //{
+        //    // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        //    var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+        //    // Add custom user claims here
+        //    userIdentity.AddClaim(new Claim("UserId", UserId));
+        //    userIdentity.AddClaim(new Claim("DisplayUserName", DisplayUserName));
+
+        //    return userIdentity;
+        //}
     }
+
 }
